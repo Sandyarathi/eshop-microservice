302db512d8406d95ea6eb0138796a851e6008bf4
SandyarathiDas <sandyarathi1212@gmail.com>
2016-10-08 17:39:04 -0700

    Docker corrections

--- api-gateway-service/src/main/java/com/apigateway/controller/GatewayController.java
+++ api-gateway-service/src/main/java/com/apigateway/controller/GatewayController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import com.apigateway.service.GatewayService;
 
 @RestController
 @RequestMapping("/gateway")
+@CrossOrigin
 public class GatewayController {
 	
 	@Autowired
